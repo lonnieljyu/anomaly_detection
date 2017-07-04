2f2b3c66383a35a6b2686bd3af9d1418f6c62e44
Lonnie Yu <lonnieljyu@gmail.com>
2017-07-04 18:38:59 -0400

    Created User class and nested data structures. Added global variables. Updated documentation.

--- src/anomaly_detection.py
+++ src/anomaly_detection.py
@@ -1,22 +1,66 @@
 import sys
 import json
+from pandas import DataFrame
 
 """
 anomaly_detection.py
+- Lonnie Yu
 
 Usage: 
 python anomaly_detection.py batch_log.json stream_log.json
 
 Description:
-Detects user's anomalous purchases based on user's social media's purchase statistics.
-Reads batch purchase records and social media friend/unfriend events and builds a social network.
-Reads stream events to check for anomalous purchases and to update the social network.
+Detects user's anomalous purchases based on user's social network's purchase statistics.
+Reads batch purchase records and social network friend/unfriend events.
+Builds users' purchase history and social networks.
+Reads stream events to check for anomalous purchases and updates record histories/social networks.
 """
 
 # Initialize global variables
-NUMBER_DEGREES = 1
-NUMBER_TRACKED_PURCHASES = 2
+NUMBER_OF_DEGREES = 1           # D, the maximum number of degrees of any connection in a social network
+NUMBER_OF_TRACKED_PURCHASES = 2 # T, the maximum sample size of tracked purchases in a social network
+CURRENT_LOG_INDEX = 0           # Used for assigning each purchase event a log index
+USERS = dict()                  # Dictionary of users
 
+# User class
+#   Contains a user id, set of friends, set of distant connections, and DataFrame of purchase history.
+#   Friends are defined as social network connections of degree 1.
+#   Distant connections are defined as social network connections of degree 2 to D.
+#   Each row of the purchase history contains one purchase event information.
+#       A DataFrame is a Pandas data structure for tabular data with easy slicing and sorting.
+class User:
+    def __init__(self, id):
+        self.id = id
+        self.friends = set()
+        self.distant_connections = set()
+        self.purchase_history = DataFrame()
+        
+    def Add_Purchase(event_dictionary):
+        self.purchase_history.append(event_dictionary)
+        
+    def Add_Friend(friend):
+        self.friends.add(friend)
+        
+    def Remove_Friend(not_friend):
+        self.friends.remove(not_friend)
+        
+    def Add_Distant_Connection(connection):
+        self.distant_connections.add(connection)
+        
+    def Remove_Distant_Connection(not_connection):
+        self.distant_connections.remove(not_connection)
+        
+    # Prints class contents for debugging
+    def print(self):
+        print(  "User Id:", self.id, 
+                "Friends:", self.friends, 
+                "Distant connections:", self.distant_connections)
+        print("Purchase history:", self.purchase_history)
+
+# Convert timestamp formatted string to datetime object.
+def Convert_To_Datetime(timestamp):
+    pass
+    
 # Create input file stream
 def Get_File_Generator(file_path):
     with open(file_path, 'r') as file:
@@ -32,12 +76,12 @@ def Process_Batch_Log(file_path):
     file_generator = Get_File_Generator(file_path)
     
     # Extract 'D' and 'T' parameters from first line
-    global NUMBER_DEGREES
-    global NUMBER_TRACKED_PURCHASES
+    global NUMBER_OF_DEGREES
+    global NUMBER_OF_TRACKED_PURCHASES
     parameters_dictionary = json.loads(file_generator.__next__())
-    NUMBER_DEGREES = int(parameters_dictionary['D'])
-    NUMBER_TRACKED_PURCHASES = int(parameters_dictionary['T'])
-    print("D =", NUMBER_DEGREES, "T =", NUMBER_TRACKED_PURCHASES)
+    NUMBER_OF_DEGREES = int(parameters_dictionary['D'])
+    NUMBER_OF_TRACKED_PURCHASES = int(parameters_dictionary['T'])
+    print("D =", NUMBER_OF_DEGREES, "T =", NUMBER_OF_TRACKED_PURCHASES)
     
     # Process each line in batch log
     for line in file_generator:
@@ -50,7 +94,7 @@ def Process_Batch_Log(file_path):
         
         # Case unfriend event
     
-# Processes stream_log.json to update data structures
+# Processes stream_log.json for updating data structures
 def Process_Stream_Log(file_path):
     print()
     print("process ", file_path)
@@ -67,8 +111,16 @@ def Process_Stream_Log(file_path):
         # Case unfriend event
     
 
-    
+# Main script environment
 if __name__ == '__main__':
     Process_Batch_Log(sys.argv[1])
     Process_Stream_Log(sys.argv[2])
+    
+    # test user class
+    print()
+    user = User(1)
+    user.print()
+    
+    user.event_type = 'new'
+    user.print()
     
